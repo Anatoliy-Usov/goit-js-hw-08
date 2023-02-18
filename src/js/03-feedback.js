22dd3cca837b540f38c4ee8f121d7993892d4fa0
Anatoliy-Usov <109044779+Anatoliy-Usov@users.noreply.github.com>
2023-02-19 01:56:02 +0200

    Update 03-feedback.js
    rewrite

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,40 +1,45 @@
 import throttle from 'lodash.throttle';
 
-const keyStorage = 'feedback-form-state';
 const checkForm = document.querySelector('.feedback-form');
-const baseForm = {};
 
 checkForm.addEventListener('submit', formSubmit);
 checkForm.addEventListener('input', throttle(formInput, 500));
+const KEY_STORAGE = 'feedback-form-state';
+const emailInput = document.querySelector('.feedback-form input');
+const messageInput = document.querySelector('.feedback-form textarea');
 
-function formInput({ target }) {
-  baseForm[target.name] = target.value;
-  localStorage.setItem(keyStorage, JSON.stringify(baseForm));
+let baseForm = {
+  email: '',
+  message: '',
+};
+
+initForm();
+
+function formInput() {
+  baseForm.email = emailInput.value;
+  baseForm.message = messageInput.value;
+  localStorage.setItem(KEY_STORAGE, JSON.stringify(baseForm));
 }
 
-function formSubmit(sub) {
-  sub.preventDefault();
+function formSubmit(evt) {
+  evt.preventDefault();
 
-  sub.currentTarget.reset();
-  localStorage.removeItem('feedback-form-state');
-  console.log(baseForm);
-}
-
-function fillingForm() {
-  const formWrite = localStorage.getItem(keyStorage);
-  let allForm;
-  try {
-    allForm = JSON.parse(formWrite);
-  } catch (error) {
-    console.log(error.message);
+  if (emailInput.value === '' || messageInput.value === '') {
+    alert('Для відправки форми мають бути заповнені всі поля!!!');
+    return;
   }
+  console.log(JSON.parse(localStorage.getItem(KEY_STORAGE)));
+  evt.currentTarget.reset();
+  localStorage.removeItem(KEY_STORAGE);
+}
 
-  if (formWrite) {
-    for (const key in allForm) {
-      let rewriteForm = checkForm.querySelector(`[name="${key}"]`);
-      rewriteForm.value = allForm[key];
-    }
+function initForm() {
+  const localStorageData = JSON.parse(localStorage.getItem(KEY_STORAGE));
+  if (localStorageData.email) {
+    emailInput.value = localStorageData.email;
+  }
+  if (localStorageData.message) {
+    messageInput.value = localStorageData.message;
   }
 }
 
-fillingForm();
