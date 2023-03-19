2c5c2d203243ad96fec8f87b231f896c1f7e6090
Anatoliy-Usov <109044779+Anatoliy-Usov@users.noreply.github.com>
2023-03-19 03:39:44 +0200

    Update 03-feedback.js
    rewrite

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,9 +4,7 @@ const checkForm = document.querySelector('.feedback-form');
 
 checkForm.addEventListener('submit', formSubmit);
 checkForm.addEventListener('input', throttle(formInput, 500));
-const KEY_STORAGE = 'feedback-form-state';
-const emailInput = document.querySelector('.feedback-form input');
-const messageInput = document.querySelector('.feedback-form textarea');
+let KEY_STORAGE = 'feedback-form-state';
 
 let baseForm = {
   email: '',
@@ -15,31 +13,34 @@ let baseForm = {
 
 initForm();
 
-function formInput() {
-  baseForm.email = emailInput.value;
-  baseForm.message = messageInput.value;
+function formInput(event) {
+  baseForm[event.target.name] = event.target.value;
   localStorage.setItem(KEY_STORAGE, JSON.stringify(baseForm));
 }
 
 function formSubmit(evt) {
   evt.preventDefault();
-
-  if (emailInput.value === '' || messageInput.value === '') {
+  if (baseForm.message === '' || baseForm.email === '') {
     alert('Для відправки форми мають бути заповнені всі поля!!!');
     return;
   }
   console.log(JSON.parse(localStorage.getItem(KEY_STORAGE)));
+  baseForm = {};
   evt.currentTarget.reset();
   localStorage.removeItem(KEY_STORAGE);
 }
 
 function initForm() {
-  const localStorageData = JSON.parse(localStorage.getItem(KEY_STORAGE));
-  if (localStorageData.email) {
-    emailInput.value = localStorageData.email;
+  let baseForm = localStorage.getItem(KEY_STORAGE);
+  if (baseForm === null) {
+    return;
   }
-  if (localStorageData.message) {
-    messageInput.value = localStorageData.message;
-  }
+  // let parsForm = JSON.parse(baseForm);
+  // for (let key in parsForm) {
+  //   checkForm.elements[key].value = parsForm[key];
+  // }
+  let parsForm = Object.entries(JSON.parse(baseForm));
+  parsForm.forEach(([key, val]) => {
+    checkForm[key].value = val;
+  });
 }
-
